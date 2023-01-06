bbd43d9dbf39feee39fb63ca2ea7608225b048b3
Splatkat <101087458+Splatkat@users.noreply.github.com>
2023-01-06 10:47:25 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tweet Sentiment Extraction',
+    author='Group40',
+    license='MIT',
+)
