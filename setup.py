dfdbfdcb0e98248b3c710efa65d0fdb4e0987b1a
Rasmus-Ulstrup <s173920@student.dtu.dk>
2023-01-16 11:42:12 +0100

    Rasmus update igen cookiecutter update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Tweet Sentiment Extraction',
-    author='Group40',
+    description='This is a test',
+    author='40',
     license='MIT',
 )
