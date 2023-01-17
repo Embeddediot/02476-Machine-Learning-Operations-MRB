118a6bf2f07e2b42c2f5712cf027818a63c83dd9
Splatkat <m@datus.dk>
2023-01-17 09:58:46 +0100

    downloads data from Kaggle

---
+++ get_data.py
@@ -0,0 +1,21 @@
+import zipfile
+import os
+
+
+from kaggle.api.kaggle_api_extended import KaggleApi
+api = KaggleApi()
+api.authenticate()
+
+path = r"./src/data"
+
+
+try:
+    api.competition_download_files("tweet-sentiment-extraction", path = path)
+except Exception:
+    print("Didn't download data")
+
+try:
+    with zipfile.ZipFile(path+"/tweet-sentiment-extraction.zip", 'r') as zip_ref:
+    zip_ref.extractall(path)
+except Exception:
+    print("Couldn't extract data from Zip-file")
