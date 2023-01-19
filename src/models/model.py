8d027513d02b02f122af0175ed66effe2534bb11
Rasmus-Ulstrup <s173920@student.dtu.dk>
2023-01-19 13:59:40 +0100

    Rasmus commit

--- src/models/model.py
+++ src/models/model.py
@@ -11,62 +11,38 @@ class TwitterSentimentAnalysis(LightningModule):
         self.tokenizer = RobertaTokenizer.from_pretrained(pretrained_model_name)
         self.model = RobertaForSequenceClassification.from_pretrained(pretrained_model_name)
         self.config = AutoConfig.from_pretrained(pretrained_model_name)
+        self.learning_rate = 2e-5
         
     def forward(self, input_ids, attention_mask):
         return self.model(input_ids, attention_mask)[0]
-    
+
     def training_step(self, batch, batch_idx):
         input_ids, attention_mask, labels = batch
         logits = self(input_ids, attention_mask)
-        loss = self.loss_fn(logits.view(-1, 2), labels.view(-1))
-        return {'loss': loss, 'log': {'train_loss': loss}}
-    
-    def validation_step(self, batch, batch_idx):
-        input_ids, attention_mask, labels = batch
-        logits = self(input_ids, attention_mask)
-        val_loss = self.loss_fn(logits.view(-1, 2), labels.view(-1))
-        preds = logits.argmax(dim=1)
-        acc = (preds == labels).float().mean()
-        return {'val_loss': val_loss, 'val_acc': acc}
-    
-    def validation_epoch_end(self, outputs):
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        avg_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
-        return {'val_loss': avg_loss, 'val_acc': avg_acc}
-    
+        loss = self.loss_fn(logits, labels)
+        
+        # Logging the loss
+        self.log('train_loss', loss)
+        return {"loss": loss}
+
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=3e-5)
+        return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
 
-    def prepare_data(self, text):
-        new_text = []
-        for t in text.split(" "):
-            t = '@user' if t.startswith('@') and len(t) > 1 else t
-            t = 'http' if t.startswith('http') else t
-            new_text.append(t)
-        return " ".join(new_text)
+    def loss_fn(self, logits, labels):
+        return torch.nn.CrossEntropyLoss()(logits, labels)
     
-    def train_dataloader(self):
-        # Return your twitter dataset dataloader here
-        pass
-
-    def val_dataloader(self):
-        # Return your twitter dataset dataloader here
-        pass
-
-
-
-
-
+    def train_epoch_end(self):
+        # Logging the metrics at the end of each epoch
+        self.log('train_loss', self.avg_loss)
 
 if __name__ == "__main__":
     model = TwitterSentimentAnalysis()
-    text = "Covid cases are increasing fast!"
-    text = model.prepare_data(text)
+    text = "I Love bjorn"
     encoded_input = model.tokenizer(text, return_tensors='pt')
     output = model(**encoded_input)
     scores = output[0].detach().numpy()
     scores = softmax(scores)
-    print(scores)
+    print(encoded_input)
 
     ranking = np.argsort(scores)
     ranking = ranking[::-1]
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,18 +4,23 @@ import torch
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
 
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+
 import pandas as pd
 import numpy as np
 
 from src.models.model import TwitterSentimentAnalysis
+from src.data.make_dataset import sentimentTweets
 
 def main():
+    ## load data
+    print("Loading data...")
+    data = sentimentTweets()
 
-    train_df = pd.read_csv("src/data/train.csv")
-    test_df = pd.read_csv("src/data/test.csv")
-
-    #pretext
-    print(train_df[0])
+    ##load model
+    print("Loading model...")
+    model = TwitterSentimentAnalysis()
 
     ## GPU
     gpus = 0
@@ -26,9 +31,43 @@ def main():
     else:
         print("Using CPU for training")
 
+    ##encoder
+    print("Encoding tokens...")
+    encoded_input = model.tokenizer.batch_encode_plus(data.train_df["text"], 
+                                                    return_tensors='pt',
+                                                    return_attention_mask=True,
+                                                    padding='max_length',
+                                                    max_length=data.train_max_len)
+    
+    #train set
+    input_ids_train = encoded_input['input_ids']
+    attention_masks_train = encoded_input['attention_mask']
+    labels_train = torch.tensor(data.train_df.label.values)
+
     ## Dataloader
+    #train set
+    dataset_train = TensorDataset(input_ids_train, 
+                                  attention_masks_train,
+                                  labels_train)
+
+    batch_size = 32
+
+    dataloader_train = DataLoader(dataset_train,
+                              sampler = RandomSampler(dataset_train),
+                              batch_size = batch_size,
+                              num_workers = os.cpu_count() or 2)
 
     ## Train
+    print("Training...")
+    trainer = Trainer(
+        max_epochs=15,
+        gpus=gpus
+    )
+
+    trainer.fit(
+        model,
+        dataloader_train
+    )
 
 
 if __name__ == "__main__":
